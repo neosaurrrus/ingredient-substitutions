b21a62e7ea77f44cbd56f583ae4dded293fe8aef
neosaurrrus <lukiek@gmail.com>
2020-05-21 00:29:41 +0100

    add basic css

--- app/models/substitution.rb
+++ app/models/substitution.rb
@@ -2,4 +2,20 @@ class Substitution < ApplicationRecord
     belongs_to :original, :class_name => :Ingredient
     belongs_to :sub, :class_name => :Ingredient
     belongs_to :user
+
+    def display
+        "<h3> <%= self.description%></h3>"
+    end
+
+    def original_ingredient
+        Ingredient.find(self.original_id)
+    end
+
+    def sub_ingredient
+        Ingredient.find(self.sub_id)
+    end
+
+    def user
+        User.find(self.user_id)
+    end
 end
