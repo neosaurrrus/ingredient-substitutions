a264ead18f484e3b56e8a320e695561704e30673
neosaurrrus <lukiek@gmail.com>
2020-05-21 23:57:41 +0100

    add sessions controller and key controller actions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(id: params[:username])
+    @user = @user.try(:authenticate, params[:password])
+    return redirect_to(controller: 'sessions', action: 'new') unless @user
+    session[:user_id] = @user.id
+    redirect_to @user
+  end
+
+  def destroy
+    if session[:user_id] 
+      session.delete :user_id
+  end
+  end
+end
