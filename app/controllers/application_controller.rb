491d17e1453ad5acc9d6c05d856898fa741138ab
neosaurrrus <lukiek@gmail.com>
2020-05-25 23:33:09 +0100

    Add auth helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
+   
     def welcome
+    end
+
+    def require_logged_in
+        return redirect_to(controller: 'sessions', action: 'new') unless current_user
+    end
+
+    def current_user
+        User.find_by(id:session[:user_id])
     end
 end
