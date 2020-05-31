ea85c527b9a66fbee393ce3c2eece514077776a0
neosaurrrus <lukiek@gmail.com>
2020-06-01 00:20:25 +0100

    Inital attempt at multiple ingredient input

--- app/controllers/substitutions_controller.rb
+++ app/controllers/substitutions_controller.rb
@@ -6,16 +6,48 @@ class SubstitutionsController < ApplicationController
   end
 
   def create
+    @new_ingredients = params[:substitution][:ingredient]
+    original_id = params[:substitution][:original_id].present?
+    sub_id = params[:substitution][:sub_id].present?
+
+
+
+
+    # New original AND Sub Ingredient
+    if !original_id && !sub_id
+      @original_ingredient = Ingredient.new(name: @new_ingredients[:name_original], description: @new_ingredients[:description_original], vegan: @new_ingredients[:vegan_original], vegetarian: @new_ingredients[:vegetarian_original], category_id: params[:substitutons[:category_id_original]], user_id: current_user.id)
+      @sub_ingredient = Ingredient.new(name: @new_ingredients[:name_sub], description: @new_ingredients[:description_sub], vegan: @new_ingredients[:vegan_sub], vegetarian: @new_ingredients[:vegetarian_sub], category_id: params[:substitutons][:category_id_sub], user_id: current_user.id)
+    
+      @original_ingredient = current_user.ingredients.create(ingredient_original_params)
+      @sub_ingredient = current_user.ingredients.create(ingredient_sub_params)
+
+      params[:substitution][:original_id] = @original_ingredient.id
+      params[:substitution][:sub_id] = @sub_ingredient.id
+
+
+    
+    # New original and EXISTING sub ingredient  
+    elsif !original_id && sub_id
+
+      @original_ingredient = Ingredient.new(name: @new_ingredients[:name_original], description: @new_ingredients[:description_original], vegan: @new_ingredients[:vegan_original], vegetarian: @new_ingredients[:vegetarian_original], category_id: params[:substitutons][:category_id_original], user_id: current_user.id)
+      @original_ingredient = current_user.ingredients.create(@original_ingredient)
+      params[:substitution][:original_id] = @original_ingredient.id
+     
+ 
+
+
+    # Existing Original and New Sub
+    elsif original_id && !sub_id
+
+     @sub_ingredient = Ingredient.new(name: @new_ingredients[:name_sub], description: @new_ingredients[:description_sub], vegan: @new_ingredients[:vegan_sub], vegetarian: @new_ingredients[:vegetarian_sub], category_id: params[:substitutons][:category_id_sub], user_id: current_user.id)
+     @sub_ingredient = current_user.ingredients.create(ingredient_sub_params)
+     params[:substitution][:sub_id] = @sub_ingredient.id
+
+
+    end
     binding.pry
-
-    # Build Original Ingredient if no original_id, set @new_original_ingredient
-    # Build subbed ingredient if no subbed_id, set @new_subbed_ingredient
-
-
-
-
     @substitution = current_user.substitutions.create(substitution_params)
-   
+
     if @substitution.save 
       redirect_to @substitution
     else 
@@ -33,8 +65,16 @@ class SubstitutionsController < ApplicationController
 
   private
     def substitution_params
-      params.require(:substitution).permit(:same_quantity,:description, :issues,:original_id, :sub_id)
-      params.require(:ingredient).permit(:name_original, :description_original, :vegan_original, :vegetarian_original, :name_subbed, :description_subbed, :vegan_subbed, :vegetarian_subbed, :category_id)
+      params.require(:substitution).permit(:same_quantity,:description, :issues, :original_id, :sub_id, :category_id_original, :category_id_sub)
       
+
+    end
+    def ingredient_original_params
+      params.require(:ingredients).permit(:name_original, :description_original, :vegan_original, :vegetarian_original, :name_sub, :description_sub, :vegan_sub, :vegetarian_sub )
+      params.require(:substitution).permit(:category_id_original)
+    end
+    def ingredient_sub_params
+      params.require(:ingredients).permit(:name_sub, :description_sub, :vegan_sub, :vegetarian_sub)
+      params.require(:substitution).permit(:category_id_sub)
     end
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,17 @@
 class Ingredient < ApplicationRecord
+    alias_attribute :name_original, :name
+    alias_attribute :name_sub, :name
+    alias_attribute :description_original, :description
+    alias_attribute :description_sub, :description
+    alias_attribute :vegan_original, :vegan
+    alias_attribute :vegan_sub, :vegan
+    alias_attribute :vegetarian_original, :vegetarian
+    alias_attribute :vegetarian_sub, :vegetarian 
+    alias_attribute :category_id_original, :category_id
+    alias_attribute :category_id_sub, :category_id
+
+
+
     has_many(:substitutions, :foreign_key => :original_id, :dependent => :destroy)
     has_many(:reverse_substitutions,:class_name => :Substitution, :foreign_key => :sub_id, :dependent => :destroy)
     has_many :ingredients, :through => :substitutions, :source => :sub 
