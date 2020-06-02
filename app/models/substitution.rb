ace20628e483348703b030844573ac93aedab55f
neosaurrrus <lukiek@gmail.com>
2020-06-03 00:38:27 +0100

    add validations to user and substitutions

--- app/models/substitution.rb
+++ app/models/substitution.rb
@@ -1,5 +1,10 @@
 class Substitution < ApplicationRecord
+    #scopes
     scope :last_5, -> { order(created_at: :desc).limit(5) }
+    #validations
+    validates :original_id, :sub_id, :description, presence: true
+
+    #associations
 
     belongs_to :original, :class_name => :Ingredient
     belongs_to :sub, :class_name => :Ingredient
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     has_secure_password
     validates_confirmation_of :password
     validates :password, length: { in: 6..20 }
-    
+    validates :username, uniqueness: true
     has_many :ingredients, :through =>:substitutions
     has_many :substitutions
     has_many :authorizations
