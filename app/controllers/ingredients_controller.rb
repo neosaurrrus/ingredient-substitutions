1c953407115bf023527b7689aba0069e88556fa9
neosaurrrus <lukiek@gmail.com>
2020-06-05 21:57:14 +0100

    new ingredient path and action

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,24 +1,51 @@
 class IngredientsController < ApplicationController
+  before_action :get_category, only: [:index, :new,:edit,:create,:update,:destroy]
 
+  def index
+  end
 
   def new
-    @ingredient = Ingredient.new(category_id: params[:category_id])
+    @ingredient = Ingredient.new(category_id: @category.id)
   end
 
   def create
-    @ingredient = Ingredient.new(ingredient_params)
-    @ingredient.save
+    @ingredient = @category.ingredients.create(ingredient_params)
+    if @ingredient.save
+
+      redirect_to category_ingredients_path
+    else 
+      render :new
+    end
+  end
+
+  def edit
+    @ingredient = Ingredient.find(params[:id])
+  end
+    
+
+  def update
+    @ingredient = Ingredient.find(params[:id])
+    @ingredient.update(ingredient_params)
     redirect_to @ingredient
   end
+
+
+
 
   def show
     @ingredient = Ingredient.find(params[:id])
   end
 
+
+
   private
 
   def ingredient_params
-    params.require(:ingredient).permit(:name, :description, :category_id, :vegan, :vegetarian)
+    params.require(:ingredient).permit(:name, :description, :category_id, :vegan, :vegetarian, :user_id)
+  end
+
+  def get_category
+    @category = Category.find_by(id: params[:category_id])
   end
 
 end
