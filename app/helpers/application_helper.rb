550c2277fc9fbb43f94df9c0ebecc45cf63bb693
neosaurrrus <lukiek@gmail.com>
2020-06-06 11:47:12 +0100

    add belongs to corrent user helper and add edit option to owner ingredients

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+    def belongs_to_current_user(instance)
+        if instance.user_id == current_user.id
+            return true
+        else
+            return false
+        end
+    end
+
+    def test_helper
+        "hello i am a helper"
+    end
 end
